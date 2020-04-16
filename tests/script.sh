8b320580cae305b545766e9c239cd3370efd539c
James A. Overton <james@overton.ca>
2020-04-16 12:32:27 -0400

    Convert script.sh to pure sh and improve

--- tests/script.sh
+++ tests/script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Don't worry about
 # - managing users
@@ -6,62 +6,63 @@
 # - logging stuff
 # - keeping secrets
 
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-IFS=$'\n\t'
 
 ### Initialization
 
+PROGNAME="$(basename "$0")"
 ROOT="$(pwd)"
 TEMP="${ROOT}/temp"
+RESULT="${TEMP}/result"
 mkdir "${TEMP}"
 export CVDB_DATA="${TEMP}/data"
-CVDB_SECRET="${CVDB_DATA}/secret"
-CVDB_STAGING="${CVDB_DATA}/staging"
-CVDB_PUBLIC="${CVDB_DATA}/public"
 EXAMPLES="${ROOT}/examples"
-CVDB=src/covicdbtools/cli.py
 
 step() {
   STEP="$1"
   echo "${STEP}"
 }
 
-# Compare two strings
-assert() {
-  MESSAGE="$1"
-  diff <(echo "$2") <(echo "$3")
-  MESSAGE=""
+fail() {
+  MESSAGE="${1:-"Unknown Error"}"
+  echo "${PROGNAME}: Failed at step '${STEP}'" 1>&2
+  echo "${PROGNAME}: ERROR ${MESSAGE}" 1>&2
+  exit 1
+}
+
+# Compare an expected string to the ${RESULT} file
+check_result() {
+  EXPECTED="$1"
+  echo "$1" | diff - "${RESULT}" || fail "expected string differs from contents of ${RESULT}"
+}
+
+# Compare an expected log file to the actual git shortlog of a branch
+check_git_log() {
+  EXPECTED="$1"
+  BRANCH="$2"
+  EXPECTED_LOG="${EXPECTED}/${BRANCH}.log"
+  ACTUAL="${CVDB_DATA}/${BRANCH}"
+  (cd "${ACTUAL}" && git shortlog HEAD) | \
+  diff "${EXPECTED_LOG}" - || fail "git shortlog for '${ACTUAL}' should match '${EXPECTED_LOG}'"
 }
 
 # Compare CVDB_DATA files and logs to a test directory
-check() {
-  MESSAGE="'${CVDB_DATA}' should match '$1/data'"
-  diff -rq --exclude=".git" "${CVDB_DATA}" "$1/data"
-  MESSAGE="'${CVDB_DATA}' git shortlog should match '$1/*.log'"
-  diff <(cd "${CVDB_SECRET}" && git shortlog HEAD) "$1/secret.log"
-  diff <(cd "${CVDB_STAGING}" && git shortlog HEAD) "$1/staging.log"
-  diff <(cd "${CVDB_PUBLIC}" && git shortlog HEAD) "$1/public.log"
-  MESSAGE=""
+check_repos() {
+  EXPECTED="$1"
+  diff -rq --exclude=".git" "${CVDB_DATA}" "${EXPECTED}/data" || fail "'${CVDB_DATA}' should match '${EXPECTED}/data'"
+  check_git_log "${EXPECTED}" secret
+  check_git_log "${EXPECTED}" staging
+  check_git_log "${EXPECTED}" public
 }
-
-# If the script exits early, print the current step.
-print_failure() {
-  echo "ERROR ${MESSAGE}"
-  echo "ERROR tests/script.sh stopped at: ${STEP}"
-}
-trap 'print_failure' INT TERM EXIT
 
 
 ### Script
 
 step "Create git data repositories for testing"
-assert "submission should fail" \
-  "$(${CVDB} initialize)" \
-  "Initialized data repositories in '${CVDB_DATA}'"
-assert "directories should be created" \
-  "$(tree "${CVDB_DATA}")" \
-  "${CVDB_DATA}
+cvdb initialize > "${RESULT}" || fail "Failed to initialize"
+check_result "Initialized data repositories in '${CVDB_DATA}'"
+
+tree "${CVDB_DATA}" > "${RESULT}" || fail "Failed to run tree"
+check_result "${CVDB_DATA}
 ├── public
 ├── secret
 └── staging
@@ -71,16 +72,16 @@ assert "directories should be created" \
 
 step "Fetch antibodies template"
 FILE="${TEMP}/antibodies-submission.xlsx"
-${CVDB} fetch template antibodies "${FILE}"
-assert "template should be empty" \
-  "$(${CVDB} read "${FILE}" Antibodies)" \
-  "Empty table"
+cvdb fetch template antibodies "${FILE}" || fail "Failed to fetch"
+cvdb read "${FILE}" Antibodies > "${RESULT}" || fail "Failed to read"
+check_result "Empty table"
 
 
 step "Submit antibodies invalid"
-assert "submission should fail" \
-  "$(${CVDB} submit antibodies "Shane Crotty" "shane@lji.org" "LJI" "${EXAMPLES}/antibodies-submission-invalid.xlsx")" \
-  "There were 6 errors
+FILE="${EXAMPLES}/antibodies-submission-invalid.xlsx" 
+cvdb submit antibodies "Shane Crotty" shane@lji.org LJI "${FILE}" \
+  > "${RESULT}" && fail "Submit invalid should fail"
+check_result "There were 6 errors
 Error in row 3: Missing required value for 'Antibody name'
 Error in row 6: Missing required value for 'Host'
 Error in row 6: 'Ig1' is not a recognized value for 'Isotype'
@@ -90,31 +91,32 @@ Error in row 9: Duplicate value 'C3' is not allowed for 'Antibody name'"
 
 
 step "Submit antibodies valid"
-assert "valid antibody submission should succeed" \
-  "$(${CVDB} submit antibodies "Shane Crotty" "shane@lji.org" "LJI" "${EXAMPLES}/antibodies-submission-valid.xlsx")" \
-  "Submitted antibodies"
-check "${ROOT}/tests/submit-antibodies"
+FILE="${EXAMPLES}/antibodies-submission-valid.xlsx" 
+cvdb submit antibodies "Shane Crotty" shane@lji.org LJI "${FILE}" \
+  > "${RESULT}" || fail "Failed to submit"
+check_result "Submitted antibodies"
+check_repos "${ROOT}/tests/submit-antibodies"
 
 
 step "Create Dataset"
-assert "staging: dataset 1 created" \
-  "$(${CVDB} create dataset "Jon Yewdell" "jyewdell@niaid.nih.gov" "neutralization")" \
-  "Created dataset 1"
-check "${ROOT}/tests/create-dataset"
+cvdb create dataset "Jon Yewdell" jyewdell@niaid.nih.gov neutralization \
+  > "${RESULT}" || fail "Failed to create"
+check_result "Created dataset 1"
+check_repos "${ROOT}/tests/create-dataset"
 
 
 step "Fetch assays template"
 FILE="${TEMP}/neutralization-submission.xlsx"
-${CVDB} fetch template 1 "${FILE}"
-assert "template should be empty" \
-  "$(${CVDB} read "${FILE}" Dataset)" \
-  "Empty table"
+cvdb fetch template 1 "${FILE}" || fail "Failed to fetch"
+cvdb read "${FILE}" Dataset > "${RESULT}" || fail "Failed to read"
+check_result "Empty table"
 
 
 step "Submit Invalid Assays"
-assert "invalid submission should fail" \
-  "$(${CVDB} submit assays "Jon Yewdell" "jyewdell@niaid.nih.gov" 1 "${EXAMPLES}/neutralization-submission-invalid.xlsx")" \
-  "There were 5 errors
+FILE="${EXAMPLES}/neutralization-submission-invalid.xlsx"
+cvdb submit assays "Jon Yewdell" jyewdell@niaid.nih.gov 1 "${FILE}" \
+  > "${RESULT}" && fail "Submit invalid should fail"
+check_result "There were 5 errors
 Error in row 2: Missing required value for 'Antibody label'
 Error in row 3: Duplicate value 'COVIC 1' is not allowed for 'Antibody label'
 Error in row 5: 'postive' is not a recognized value for 'Qualitative measure'
@@ -123,31 +125,29 @@ Error in row 6: 'intermediate' is not a recognized value for 'Qualitative measur
 
 
 step "Submit Valid Assays"
-assert "valid submission should succeed" \
-  "$(${CVDB} submit assays "Jon Yewdell" "jyewdell@niaid.nih.gov" 1 "${EXAMPLES}/neutralization-submission-valid.xlsx")" \
-  "Submitted assays to dataset 1"
-check "${ROOT}/tests/submit-assays"
+FILE="${EXAMPLES}/neutralization-submission-valid.xlsx"
+cvdb submit assays "Jon Yewdell" jyewdell@niaid.nih.gov 1 "${FILE}" \
+  > "${RESULT}" || fail "Failed to submit"
+check_result "Submitted assays to dataset 1"
+check_repos "${ROOT}/tests/submit-assays"
 
 
 step "Promote Dataset"
-assert "promotion should succeed" \
-  "$(${CVDB} promote dataset "Sharon Schendel" "schendel@lji.org" 1)" \
-  "Promoted dataset 1 from staging to public"
-check "${ROOT}/tests/promote-dataset"
+cvdb promote dataset "Sharon Schendel" "schendel@lji.org" 1 \
+  > "${RESULT}" || faile "Failed to promote"
+check_result "Promoted dataset 1 from staging to public"
+check_repos "${ROOT}/tests/promote-dataset"
 
 
 #step "Update Dataset"
-#${CVDB} submit assays "${EXAMPLES}/neutralization-submission-valid-update.xlsx"
+#cvdb submit assays "${EXAMPLES}/neutralization-submission-valid-update.xlsx"
 # TODO: check repos (files and git log): staging, public
 # TODO: build and check SQL tables
 
 
 #step "Promote Updated Dataset"
-#${CVDB} promote dataset 1
+#cvdb promote dataset 1
 # TODO: check repos (files and git log): staging, public
 # TODO: build and check SQL tables: staging, public
 
-
-# Clear trap to exit cleanly
-trap '' INT TERM EXIT
 echo "SUCCESS!!!"
